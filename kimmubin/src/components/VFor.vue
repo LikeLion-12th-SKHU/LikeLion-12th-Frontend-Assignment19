247d37bc0f5da1c737bf7a9aae94219bfaa1397a
Kim, mubin <kimmubin0318@gmail.com>
2024-11-29 09:52:30 +0900

    19주차 Assignment - 김무빈

---
+++ kimmubin/src/components/VFor.vue
@@ -0,0 +1,25 @@
+<script setup>
+const example = ["순서대로 출력", "index[0]", "index[1]", "index[2]"];
+</script>
+
+<template>
+  <ul>
+    <li v-for="(name, index) in example" :key="index">
+      {{ name }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  background-color: #304a6e;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #c0d0ef;
+}
+</style>
---
+++ kimmubin/src/components/VShow.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+const isRed = ref(true);
+</script>
+
+<template>
+  <div>
+    <p v-show="isRed">보이거나 숨길 수 있습니당</p>
+    <button @click="isRed = !isRed">표시/숨기기</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  background-color: #304a6e;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #c0d0ef;
+}
+</style>
